fc6bcf61bcd4accd40b40e06122c8b11012d6459
Ivan Galindez <ivangfz98@gmail.com>
2021-11-25 16:00:22 -0500

    creacion frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule} from '@angular/forms';
+//import { HttpModule} from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { InmobiliariaComponent } from './inmobiliaria/inmobiliaria.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+
+const router: Routes=[{
+  path:'user',
+  component: UserComponent,
+},{
+  path:'inmo',
+  component: InmobiliariaComponent
+},{
+  path:'ubi',
+  component: UbicacionComponent
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    InmobiliariaComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(router)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
